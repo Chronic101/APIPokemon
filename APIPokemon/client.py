f79bba8d47a0fe113cf57f1debcbdca3398aaa77
Alex Morinigo <153045735+Chronic101@users.noreply.github.com>
2026-02-16 15:30:08 -0300

    Delete APIPokemon directory

--- APIPokemon/client.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient().local
-
-db = client["pokedb"]
-
-db_pokemon = db["pokemon"]
--- APIPokemon/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from app.routers import pokemon_router
-
-app = FastAPI()
-
-app.include_router(pokemon_router.router)
